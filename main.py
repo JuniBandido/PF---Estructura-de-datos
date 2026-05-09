1c9380372699a52b39f5d843fcd687061cebceae
José Noguera <juniornoguera1@gmail.com>
2026-05-09 01:18:57 -0600

    Nodos para cada tipo

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#Principal
+#Logica
+class NodoBST:
+    def __init__(self, v):
+        self.val  = v
+        self.izq  = None
+        self.der  = None
+
+
+class NodoAVL:
+    def __init__(self, v):
+        self.val   = v
+        self.izq   = None
+        self.der   = None
+        self.altura = 1
