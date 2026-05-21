e9c6cef29fc0a3980a5a9bfca82b2d4d1259b44f
José Noguera <juniornoguera1@gmail.com>
2026-05-20 19:47:07 -0600

    main - archivo que mezcla todo

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #Principal
+
+import sys
+from PyQt5.QtWidgets import QApplication
+from ui import FlowTree
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyle("Fusion")
+    ventana = FlowTree()
+    ventana.show()
+    sys.exit(app.exec_())
